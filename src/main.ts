35e0f71a54ec787ba3e591bd1e3233537d4cdacf
okathira <43328895+okathira@users.noreply.github.com>
2023-11-29 03:39:26 +0900

    ソースキャンバスのアニメーションを別ファイルに切り出す

--- src/main.ts
+++ src/main.ts
@@ -1,50 +1,16 @@
+import { getAnimateCanvasFunc } from "./animateCanvas";
 import type { VideoWorkerMessage } from "./videoWorker";
-
-// 表示する絵文字一覧
-const foods = ["🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭"];
-
-const getRandomFood = () => {
-  const index = Math.floor(Math.random() * foods.length);
-  return foods[index];
-};
 
 // Draw pretty animation on the source canvas
 const startDrawing = async () => {
   // エンコード元となるキャンバス
   const cnv = <HTMLCanvasElement>document.getElementById("src");
-  const ctx = cnv.getContext("2d");
 
-  if (!ctx) return;
-
-  // キャンバスへの描画に関する設定
-  ctx.fillStyle = "#fff5e6";
-  const width = cnv.width;
-  const height = cnv.height;
-  const cx = width / 2;
-  const cy = height / 2;
-  // const r = Math.min(width, height) / 5;
-  ctx.font = "30px Helvetica";
-  const text = getRandomFood() + "📹📷Hello WebCodecs 🎥🎞️" + getRandomFood();
-  const size = ctx.measureText(text).width;
+  const animateCanvas = getAnimateCanvasFunc(cnv);
 
   // １フレーム描画する
   const drawOneFrame: FrameRequestCallback = (time) => {
-    const angle = Math.PI * 2 * (time / 5000);
-    const scale = 1 + 0.3 * Math.sin(Math.PI * 2 * (time / 7000));
-    ctx.save();
-    ctx.fillRect(0, 0, width, height);
-
-    ctx.translate(cx, cy);
-    ctx.rotate(angle);
-    ctx.scale(scale, scale);
-
-    ctx.fillStyle = `hsl(${angle * 40},80%,50%)`;
-    ctx.fillRect(-size / 2, 10, size, 25);
-
-    ctx.fillStyle = "black";
-    ctx.fillText(text, -size / 2, 0);
-
-    ctx.restore();
+    animateCanvas(time);
 
     // 描画タイミングに合わせて毎フレーム描画する
     window.requestAnimationFrame(drawOneFrame);
