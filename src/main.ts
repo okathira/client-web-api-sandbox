60b45a68a870f01db3918ccc26c17f21840c6abe
okathira <43328895+okathira@users.noreply.github.com>
2023-12-01 03:04:52 +0900

    feat: Canvas要素をスクリプトで生成するように

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { getAnimateCanvasFunc } from "./animateCanvas";
 import type { VideoWorkerMessage } from "./videoWorker";
 
-// Draw pretty animation on the source canvas
-const startDrawing = async () => {
-  // エンコード元となるキャンバス
-  const cnv = <HTMLCanvasElement>document.getElementById("src");
+const CANVAS_WIDTH = 640;
+const CANVAS_HEIGHT = 480;
+const FPS = 60;
 
+// Draw pretty animation on the source canvas
+const startDrawing = async (app: HTMLElement) => {
+  // エンコード元となるキャンバス
+  const cnv = document.createElement("canvas");
+  cnv.width = CANVAS_WIDTH;
+  cnv.height = CANVAS_HEIGHT;
+  cnv.id = "src";
+  app.appendChild(cnv);
+
+  // キャンバスの描画を行う関数
   const animateCanvas = getAnimateCanvasFunc(cnv);
 
   // １フレーム描画する
@@ -16,60 +25,62 @@ const startDrawing = async () => {
     window.requestAnimationFrame(drawOneFrame);
   };
   window.requestAnimationFrame(drawOneFrame);
+
+  return cnv;
 };
 
 // WebCodecsの処理
-const startWorker = () => {
+const startWorker = async (app: HTMLElement, srcCanvas: HTMLCanvasElement) => {
   const worker = new Worker(new URL("./videoWorker.ts", import.meta.url), {
     name: "Video worker",
   });
 
-  worker.onmessage = (e) => {
-    // Recreate worker in case of an error
-    console.log("Worker error: " + e.data);
-    worker.terminate();
-    startWorker();
-  };
-
-  // Capture animation track for the source canvas
-  const src_cnv = <HTMLCanvasElement>document.getElementById("src");
-  if (!src_cnv) return;
-
-  const fps = 60;
-  const stream = src_cnv?.captureStream(fps);
+  const stream = srcCanvas.captureStream(FPS);
   const track = stream.getVideoTracks()[0];
   const media_processor = new MediaStreamTrackProcessor({ track });
   const reader = media_processor.readable;
 
   // Create a new destination canvas
   const dst_cnv = document.createElement("canvas");
-  dst_cnv.width = src_cnv.width;
-  dst_cnv.height = src_cnv.height;
-  const dst = document.getElementById("dst");
+  dst_cnv.width = srcCanvas.width;
+  dst_cnv.height = srcCanvas.height;
+  dst_cnv.id = "dst";
+  app.appendChild(dst_cnv);
 
-  if (!dst) return;
-
-  if (dst.firstChild) dst.removeChild(dst.firstChild);
-  dst.appendChild(dst_cnv);
   // workerにキャンバスの制御を譲渡する
   const offscreen = dst_cnv.transferControlToOffscreen();
 
   const message: VideoWorkerMessage = {
     canvas: offscreen,
     frame_source: reader,
-    fps: fps,
+    fps: FPS,
   };
   worker.postMessage(message, [offscreen, reader]);
+
+  // workerからメッセージあったらエラーが起きているのでworkerを再起動する
+  worker.onmessage = (e) => {
+    // Recreate worker in case of an error
+    console.log("Worker error: " + e.data);
+    worker.terminate();
+
+    // エラーが起きたタイミングでキャンバスを削除しておけば重複気にしなくて良い気がする
+    document.getElementById("dst")?.remove();
+
+    startWorker(app, srcCanvas);
+  };
 };
 
-const main = () => {
+const main = async () => {
   if (!("VideoFrame" in window)) {
     document.body.innerHTML = "<h1>WebCodecs API is not supported.</h1>";
     return;
   }
 
-  startDrawing();
-  startWorker();
+  const app = document.getElementById("app");
+  if (!app) throw new Error("Could not find app element");
+
+  const srcCanvas = await startDrawing(app);
+  startWorker(app, srcCanvas);
 };
 
 document.body.onload = main;
